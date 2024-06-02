fc38f7e0e6183d758535dc286ed5c2992728f64c
OnSystem <onsystem@proton.me>
2024-06-02 03:13:58 +0200

    feature: tests correct replace version in maven

--- src/proccess.ts
+++ src/proccess.ts
@@ -1,34 +1,23 @@
 import {ApplicationType, Command, UpdateVersionMode} from '@action-update-version/command';
 import * as fs from 'fs';
-import * as path from 'path';
+import {scan_paths_and_filter_by_file_name} from "@action-update-version/fs";
 
 const target_file_application = {
     maven: "pom.xml",
 }
-export const version_by_application = {
+export const get_version_by_application = {
     maven: (content_file: string): string => require('xml-js').xml2js(content_file, {compact: true}).project.version._text,
 }
 
-export const create_new_content = {
-    "maven": (content_file: string, version: string): string => require('xml-js').js2xml(
-        require('xml-js').xml2js(content_file, {compact: true}),
-        {compact: true, spaces: 4})
+const create_new_content = {
+    "maven": (content_file: string, version: string): string => generate_new_content_maven(content_file, version),
 }
 
-/*
-export const regex_by_mode_version = {
-    major: /^(\d+)/,
-    minor: /^\d+\.(\d+)\.\d+/,
-    patch: /^\d+\.\d+\.(\d+)/,
-    "pr-snapshot": /-SNAPSHOT-(.*)/,
-}
-*/
-
-const regex_take_version_number = /^(\d+)\.(\d+)\.(\d+)(?:-.*)?$/
+const REGEX_TAKE_VERSION_DATA = /^(\d+)\.(\d+)\.(\d+)(?:-.*)?$/
 
 function execute_process(command: Command) {
     const target = target_file_application[command.application_type];
-    const path_files = scan_path_by_name_file(command.path, target);
+    const path_files = scan_paths_and_filter_by_file_name(command.path, target);
     for (const path of path_files) {
         const content = fs.readFileSync(path, 'utf-8');
         const new_content = generate_new_content(content, command.application_type, command.update_version_mode)
@@ -38,12 +27,12 @@ function execute_process(command: Command) {
 }
 
 
-function generate_new_content(
+export function generate_new_content(
     content: string,
     application_type: ApplicationType,
     update_version_mode: UpdateVersionMode
 ) {
-    const fn_get_version_file_content = version_by_application[application_type];
+    const fn_get_version_file_content = get_version_by_application[application_type];
     const fn_create_new_content = create_new_content[application_type];
 
 
@@ -54,11 +43,9 @@ function generate_new_content(
 
 
 export function generate_new_version(version: string, update_version_mode: UpdateVersionMode): string {
-    const matcher = version.match(regex_take_version_number);
+    const matcher = version.match(REGEX_TAKE_VERSION_DATA);
 
     if (matcher) {
-        //const number_version_to_change_by_mode = (parseInt(matcher[0]) + 1).toString();
-        //return version.replace(regExp, number_version_to_change_by_mode);
 
         switch (update_version_mode) {
             case "major":
@@ -76,26 +63,14 @@ export function generate_new_version(version: string, update_version_mode: Updat
 }
 
 
-function scan_path_by_name_file(path_directory_or_file: string, name_file: string): string[] {
-    let archivosEncontrados: string[] = [];
-    const stats = fs.statSync(path_directory_or_file);
-    if (stats.isDirectory()) {
-        const archivos = fs.readdirSync(path_directory_or_file);
-        archivos.forEach(value => {
-            const rutaArchivo = path.join(path_directory_or_file, value);
-            const inner = fs.statSync(rutaArchivo);
-            if (inner.isDirectory()) {
-                archivosEncontrados = archivosEncontrados.concat(scan_path_by_name_file(rutaArchivo, name_file));
-            } else if (inner.isFile() && value === name_file) {
-                archivosEncontrados.push(fs.realpathSync(value));
-            }
-        });
-    } else if (stats.isFile() && path_directory_or_file === name_file) {
-        archivosEncontrados.push(fs.realpathSync(path_directory_or_file));
-    }
+function generate_new_content_maven(content_file: string, version: string): string {
+    const create_json_content = require('xml-js')
+        .xml2js(content_file, {compact: true, explicitArray: false});
+    create_json_content.project.version._text = version
 
-    return archivosEncontrados
+    return require('xml-js').js2xml(
+        create_json_content, {compact: true, spaces: 4}
+    )
 }
-
 
 export default execute_process;
--- test/maven.test.ts
+++ test/maven.test.ts
@@ -1,48 +1,98 @@
-import {version_by_application} from '@action-update-version/proccess';
+import {generate_new_content, get_version_by_application} from '@action-update-version/proccess';
 
 
 test('Test case get version maven', () => {
-    const fn_get_version = version_by_application["maven"];
-    const version = fn_get_version(pom);
-    expect(version).toEqual("0.0.1-SNAPSHOT")
+    const fn_get_version = get_version_by_application["maven"];
+    const version_0_0_1_snapshot = fn_get_version(pom_0_0_1_snapshot);
+    const version_0_0_2 = fn_get_version(pom_0_0_2)
+    const version_0_1_0 = fn_get_version(pom_0_1_0)
+    const version_1_0_0 = fn_get_version(pom_1_0_0)
+
+
+    expect(version_0_0_1_snapshot).toEqual("0.0.1-SNAPSHOT")
+    expect(version_0_0_2).toEqual("0.0.2")
+    expect(version_0_1_0).toEqual("0.1.0")
+    expect(version_1_0_0).toEqual("1.0.0")
+
 });
 
 test('Test update version maven major', () => {
-
+    const new_content_major_version = generate_new_content(pom_0_0_1_snapshot, "maven", "major");
+    expect(new_content_major_version).toEqual(pom_1_0_0)
 });
 
+test('Test update version maven minor', () => {
+    const new_content_minor_version = generate_new_content(pom_0_0_1_snapshot, "maven", "minor");
+    expect(new_content_minor_version).toEqual(pom_0_1_0)
+});
 
-const pom = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-    "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-    "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
-    "    <modelVersion>4.0.0</modelVersion>\n" +
-    "    <parent>\n" +
-    "        <groupId>org.springframework.boot</groupId>\n" +
-    "        <artifactId>spring-boot-starter-parent</artifactId>\n" +
-    "        <version>3.2.3</version>\n" +
-    "        <relativePath/> <!-- lookup parent from repository -->\n" +
-    "    </parent>\n" +
-    "    <groupId>com.onsystem</groupId>\n" +
-    "    <artifactId>example</artifactId>\n" +
-    "    <version>0.0.1-SNAPSHOT</version>\n" +
-    "    <name>example</name>\n" +
-    "    <description>code</description>\n" +
-    "    <properties>\n" +
-    "        <java.version>21</java.version>\n" +
-    "    </properties>\n" +
-    "    <dependencies>\n" +
-    "        <dependency>\n" +
-    "            <groupId>org.example</groupId>\n" +
-    "            <artifactId>example</artifactId>\n" +
-    "            <optional>true</optional>\n" +
-    "            <version>2.16.1</version>\n" +
-    "        </dependency>\n" +
-    "    </dependencies>\n" +
-    "\n" +
-    "    <build>\n" +
-    "        <plugins>\n" +
-    "\n" +
-    "        </plugins>\n" +
-    "    </build>\n" +
-    "\n" +
-    "</project>\n"
+test('Test update version maven patch', () => {
+    const new_content_patch_version = generate_new_content(pom_0_0_1_snapshot, "maven", "patch");
+    expect(new_content_patch_version).toEqual(pom_0_0_2)
+});
+
+const pom_0_0_1_snapshot = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
+    '         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">\n' +
+    '    <modelVersion>4.0.0</modelVersion>\n' +
+    '    <groupId>com.onsystem</groupId>\n' +
+    '    <artifactId>example-artifact</artifactId>\n' +
+    '    <version>0.0.1-SNAPSHOT</version>\n' +
+    '    <name>example</name>\n' +
+    '    <description>example_code</description>\n' +
+    '    <properties>\n' +
+    '        <java.version>21</java.version>\n' +
+    '    </properties>\n' +
+    '    <dependencies/>\n'+
+    '    <build>\n' +
+    '        <plugins/>\n' +
+    '    </build>\n' +
+    '</project>'
+const pom_0_0_2 = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">\n' +
+    '    <modelVersion>4.0.0</modelVersion>\n' +
+    '    <groupId>com.onsystem</groupId>\n' +
+    '    <artifactId>example-artifact</artifactId>\n' +
+    '    <version>0.0.2</version>\n' +
+    '    <name>example</name>\n' +
+    '    <description>example_code</description>\n' +
+    '    <properties>\n' +
+    '        <java.version>21</java.version>\n' +
+    '    </properties>\n' +
+    '    <dependencies/>\n' +
+    '    <build>\n' +
+    '        <plugins/>\n' +
+    '    </build>\n' +
+    '</project>'
+const pom_1_0_0 = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">\n' +
+    '    <modelVersion>4.0.0</modelVersion>\n' +
+    '    <groupId>com.onsystem</groupId>\n' +
+    '    <artifactId>example-artifact</artifactId>\n' +
+    '    <version>1.0.0</version>\n' +
+    '    <name>example</name>\n' +
+    '    <description>example_code</description>\n' +
+    '    <properties>\n' +
+    '        <java.version>21</java.version>\n' +
+    '    </properties>\n' +
+    '    <dependencies/>\n' +
+    '    <build>\n' +
+    '        <plugins/>\n' +
+    '    </build>\n' +
+    '</project>'
+const pom_0_1_0 = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">\n' +
+    '    <modelVersion>4.0.0</modelVersion>\n' +
+    '    <groupId>com.onsystem</groupId>\n' +
+    '    <artifactId>example-artifact</artifactId>\n' +
+    '    <version>0.1.0</version>\n' +
+    '    <name>example</name>\n' +
+    '    <description>example_code</description>\n' +
+    '    <properties>\n' +
+    '        <java.version>21</java.version>\n' +
+    '    </properties>\n' +
+    '    <dependencies/>\n' +
+    '    <build>\n' +
+    '        <plugins/>\n' +
+    '    </build>\n' +
+    '</project>'
